02cf2a8bc09d892173a89308c8ee098c91a107ce
Pat Allan <pat@freelancing-gods.com>
2008-07-02 11:48:58 -0400

    Specs for tagging model using tagging path

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -9,4 +9,16 @@ describe Tagging do
   it "should create a new instance given valid attributes" do
     Tagging.create!(@valid_attributes)
   end
+  
+  it "should return a TaggingPath instance for path" do
+    Tagging.new(:path => "_1_2_").path.should be_kind_of(TaggingPath)
+  end
+  
+  it "should accept TaggingPaths or Strings for path" do
+    tagging = Tagging.new(:path => "_1_2_")
+    tagging.path = "_1_3_4_"
+    tagging.path.to_s.should == "_1_3_4_"
+    tagging.path = TaggingPath.new("_1_4_3_")
+    tagging.path.to_s.should == "_1_4_3_"
+  end
 end
