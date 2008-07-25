53b93db7a3049cce456dabcadfe6022cffd04a0f
Gregory Mirzayantz <gm@nguma.com>
2008-07-25 14:41:23 -0400

    sanitizing

--- app/helpers/nuniverse_helper.rb
+++ app/helpers/nuniverse_helper.rb
@@ -31,16 +31,38 @@ module NuniverseHelper
 	def link_to_object(object, params = {})
 		case object.service
 		when "amazon"
-			url = h "/ws/show?service=amazon&item=#{CGI::escape(object.url)}"
+			url = h "/ws/show?service=amazon&item=#{CGI::escape(object.url.rstrip)}"
 		when "ebay"
-			url = h "/ws/show?service=ebay&item=#{CGI::escape(object.url)}"
+			url = h "/ws/show?service=ebay&item=#{CGI::escape(object.url.rstrip)}"
 		when "video"
-			url = h "/ws/show?service=video&item=#{CGI::escape(object.url)}&flashvars=#{CGI::escape(object.flashvars)}"
+			url = h "/ws/show?service=video&item=#{CGI::escape(object.url.rstrip)}&flashvars=#{CGI::escape(object.flashvars)}"
 		else
 			url = "/section_of/#{params[:path]}"
 		end
 		
-		link_to(object.name.capitalize, url, :class =>"main inner")
+		link_to(object.name.capitalize, url, :class =>"inner")
+	end
+	
+	def link_to_ws_object(object, params = {})
+		case params[:service]
+		when "video"
+			url = h "/ws/show?service=video&item=#{object.playUrl}"
+			title = object.titleNoFormatting
+		when "google"
+			title = object.titleNoFormatting
+			url = h "/ws/show?service=google&item=#{object.url.rstrip}"
+			dom_class = ""
+		when "amazon"
+			title = h sanitize(object.title[0..100])
+			url = h "/ws/show?service=amazon&item=#{object.id.rstrip}"
+		when "ebay"
+			title = h sanitize(object.title[0..100])
+			url = h "/ws/show?service=ebay&item=#{object.item_id.rstrip}"
+		else
+			title = h object.title
+			url = h "/ws/show?service=#{params[:service]}&item=#{object.url}"
+		end
+		link_to(title.capitalize, url, :class => dom_class || "inner")
 	end
 	
 end
