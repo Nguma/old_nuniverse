567a1e9091e97a0dd328a6eaca21bf467b57179d
Pat Allan <pat@freelancing-gods.com>
2008-07-30 15:45:22 -0400

    Fixing avatars controller to delete old avatars

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -8,6 +8,10 @@ class AvatarsController < ApplicationController
     @avatar.tag_id = params[:tag_id]
     
     if @avatar.save
+      Avatar.find(:all, :conditions => {:tag_id => params[:tag_id]}).each do |av|
+        av.destroy unless av.id == @avatar.id
+      end
+      
       redirect_to tag_path(@avatar.tag)
     else
       puts @avatar.errors.inspect
