8eda46789f984d1b3766d0225821ab5c84be9ac5
Gregory Mirzayantz <gm@nguma.com>
2008-09-13 13:21:49 -0400

    more fix for indexing and migrtions

--- db/migrate/20080908221548_readd_tagging_kind.rb
+++ db/migrate/20080908221548_readd_tagging_kind.rb
@@ -1,8 +1,9 @@
 class ReaddTaggingKind < ActiveRecord::Migration
   def self.up
 		#add_column :taggings, :kind, :string
-		remove_index :taggings, [:subject_id, :object_id, :user_id]
-		add_index :taggings, [:subject_id, :user_id, :kind], :unique => true
+		#remove_index :taggings, [:subject_id, :object_id, :user_id]
+		add_index :taggings, [:subject_id, :user_id, :kind]
+		add_index :taggings, [:subject_id, :user_id, :object_id, :kind], :unique => true, :name => 'unique_entries_index'
   end
 
   def self.down
