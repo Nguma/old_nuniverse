4f1ff7778f28e4043a2775264bbe577fea84acf8
Gregory Mirzayantz <gm@nguma.com>
2008-12-12 12:40:32 -0500

    disabled migrations

--- db/migrate/20081209203651_make_tags_polymorphic.rb
+++ db/migrate/20081209203651_make_tags_polymorphic.rb
@@ -5,43 +5,43 @@ class MakeTagsPolymorphic < ActiveRecord::Migration
 		
 	#	remove_column :tags, :related_date
 		
-		tags = Tag.find(:all, :conditions => 'taggable_type = "nuniverse"')
-		tags.each do |tag|
-			n = Nuniverse.create(:name => tag.label)
-			tag.taggable_type = "Nuniverse"
-			tag.taggable_id = n.id
-		end
-
-		tags = Tag.find(:all, :conditions => 'taggable_type = "bookmark"')
-		tags.each do |tag|
-			n = Bookmark.create(:name => tag.label, :url => tag.url, :service => tag.service )
-			tag.taggable_type = "Bookmark"
-			tag.taggable_id = n.id
-		end	
-		
-		tags = Tag.find(:all, :conditions => 'taggable_type = "video"')
-		tags.each do |tag|
-			n = Video.create(:name => tag.label,  :url => tag.url, :service => tag.service )
-			tag.taggable_type = "Video"
-			tag.taggable_id = n.id
-		end
-		
-		tags = Tag.find(:all, :conditions => 'taggable_type = "image"')
-		tags.each do |tag|
-			n = Image.find(:filename => tag.label)
-			tag.taggable_type = "Image"
-			tag.taggable_id = n.id
-		end
-		
+		# tags = Tag.find(:all, :conditions => 'taggable_type = "nuniverse"')
+		# 		tags.each do |tag|
+		# 			n = Nuniverse.create(:name => tag.label)
+		# 			tag.taggable_type = "Nuniverse"
+		# 			tag.taggable_id = n.id
+		# 		end
+		# 
+		# 		tags = Tag.find(:all, :conditions => 'taggable_type = "bookmark"')
+		# 		tags.each do |tag|
+		# 			n = Bookmark.create(:name => tag.label, :url => tag.url, :service => tag.service )
+		# 			tag.taggable_type = "Bookmark"
+		# 			tag.taggable_id = n.id
+		# 		end	
+		# 		
+		# 		tags = Tag.find(:all, :conditions => 'taggable_type = "video"')
+		# 		tags.each do |tag|
+		# 			n = Video.create(:name => tag.label,  :url => tag.url, :service => tag.service )
+		# 			tag.taggable_type = "Video"
+		# 			tag.taggable_id = n.id
+		# 		end
+		# 		
+		# 		tags = Tag.find(:all, :conditions => 'taggable_type = "image"')
+		# 		tags.each do |tag|
+		# 			n = Image.find(:filename => tag.label)
+		# 			tag.taggable_type = "Image"
+		# 			tag.taggable_id = n.id
+		# 		end
+		# 		
   end
 
   def self.down
-		rename_column :tags, :taggable_type, :kind
-		remove_column :tags, :taggable_id
-		
-		drop_table :nuniverses
-		drop_table :bookmarks
-		drop_table :locations
-		drop_table :videos
+		# rename_column :tags, :taggable_type, :kind
+		# 		remove_column :tags, :taggable_id
+		# 		
+		# 		drop_table :nuniverses
+		# 		drop_table :bookmarks
+		# 		drop_table :locations
+		# 		drop_table :videos
   end
 end
