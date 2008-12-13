c82d84786c94ffdad8c45218b654e572da306ee0
Gregory Mirzayantz <gm@nguma.com>
2008-12-13 13:45:56 -0500

    added mongrel_Conf var to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,10 +5,12 @@ default_run_options[:pty] = true
 set :application, "nuniverse"
 set :repository,  "git@github.com:Nguma/nuniverse.git"
 
+
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/var/www/apps/#{application}"
+set :mongrel_conf, "#{deploy_to}/current/config/mongrel_cluster.yml"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
