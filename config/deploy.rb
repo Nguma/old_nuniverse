dfe56f005fddeef2c4a62cf9da8af09f3a893ad8
Gregory Mirzayantz <gm@nguma.com>
2008-12-13 13:48:07 -0500

    reverted

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,37 +55,37 @@ namespace :deploy do
   # task :after_update do
   #    # sudo "cp #{release_path}/config/database.example.yml #{release_path}/config/database.yml"
   #  end
-	namespace :mongrel do
-	    [ :stop, :start, :restart ].each do |t|
-	      desc "#{t.to_s.capitalize} the mongrel appserver"
-	      task t, :roles => :app do
-	        #invoke_command checks the use_sudo variable to determine how to run the mongrel_rails command
-	        invoke_command "mongrel_rails cluster::#{t.to_s} -C #{mongrel_conf}", :via => run_method
-	      end
-	    end
-	  end
-
-	  desc "Custom restart task for mongrel cluster"
-	  task :restart, :roles => :app, :except => { :no_release => true } do
-	    deploy.mongrel.restart
-	  end
-
-	  desc "Custom start task for mongrel cluster"
-	  task :start, :roles => :app do
-	    deploy.mongrel.start
-	  end
-
-	  desc "Custom stop task for mongrel cluster"
-	  task :stop, :roles => :app do
-	    deploy.mongrel.stop
-	  end
+	# namespace :mongrel do
+	#     [ :stop, :start, :restart ].each do |t|
+	#       desc "#{t.to_s.capitalize} the mongrel appserver"
+	#       task t, :roles => :app do
+	#         #invoke_command checks the use_sudo variable to determine how to run the mongrel_rails command
+	#         invoke_command "mongrel_rails cluster::#{t.to_s} -C #{mongrel_conf}", :via => run_method
+	#       end
+	#     end
+	#   end
+	# 
+	#   desc "Custom restart task for mongrel cluster"
+	#   task :restart, :roles => :app, :except => { :no_release => true } do
+	#     deploy.mongrel.restart
+	#   end
+	# 
+	#   desc "Custom start task for mongrel cluster"
+	#   task :start, :roles => :app do
+	#     deploy.mongrel.start
+	#   end
+	# 
+	#   desc "Custom stop task for mongrel cluster"
+	#   task :stop, :roles => :app do
+	#     deploy.mongrel.stop
+	#   end
 
 	
 	
-  # task :restart do
-  #   stop
-  #   start
-  # end
+  task :restart do
+    stop
+    start
+  end
     
 
 	task :after_update, :roles => :app do
