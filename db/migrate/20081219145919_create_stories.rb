f22fe98e497377fd869c8a288917a55b2a3400fc
Gregory Mirzayantz <gm@nguma.com>
2008-12-30 09:51:11 +0100

    disabled stories migrations for matching deploy

--- db/migrate/20081219145919_create_stories.rb
+++ db/migrate/20081219145919_create_stories.rb
@@ -1,13 +1,13 @@
 class CreateStories < ActiveRecord::Migration
   def self.up
-    create_table :stories do |t|
-			t.column :name, :string
-			t.column :author_id, :integer
-			t.column :parent_id, :integer
-			t.column :public, :boolean, :default => 1
-      t.timestamps
-    end
-		add_index :stories, :author_id
+    # create_table :stories do |t|
+    # 			t.column :name, :string
+    # 			t.column :author_id, :integer
+    # 			t.column :parent_id, :integer
+    # 			t.column :public, :boolean, :default => 1
+    #      t.timestamps
+    #    end
+		# add_index :stories, :author_id
   end
 
   def self.down
